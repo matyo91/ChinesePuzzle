31c1d7ea381e18bf4ae1c93d4392ccc0179c7dda
Mathieu Ledru <mathieu.ledru@twenga.com>
2024-05-30 17:46:28 +0200

    Add default scenes

---
+++ Godot/scenes/game.gd
@@ -0,0 +1,9 @@
+class_name GameScene extends Node2D
+
+var root: RootScene
+
+func _ready() -> void:
+	pass
+
+func update_layout():
+	pass
---
+++ Godot/scenes/root.gd
@@ -0,0 +1,44 @@
+class_name RootScene extends Node2D
+
+@onready var viewport = get_viewport()
+
+var current_scene = null
+var soundManager: SoundManager
+
+var scenes = {
+	"game": preload("res://scenes/game.tscn"),
+}
+
+func _init():
+	soundManager = SoundManager.new()
+	soundManager.root = self
+
+func _ready() -> void:
+	viewport.connect("size_changed", size_changed)
+	
+	add_child(soundManager)
+	
+	set_current_scene("game")
+	
+	size_changed()
+
+func set_current_scene(scene: String):
+	if current_scene != null:
+		remove_child(current_scene)
+	
+	current_scene = scenes[scene].instantiate()
+	current_scene.root = self
+	add_child(current_scene)
+	
+	size_changed()
+
+func size_changed():
+	var size = Vector2(512, 342)
+	
+	var current_size = DisplayServer.window_get_size()
+	
+	var scale_factor = Vector2(current_size.x / size.x, current_size.y / size.y)
+	set_scale(scale_factor)
+	
+	if current_scene != null:
+		current_scene.update_layout()
